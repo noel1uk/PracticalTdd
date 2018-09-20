9d84f18350a15309d20492f94594e28feb7e3848
Noel Vock <noel1uk@gmail.com>
2018-09-20 23:00:29 +0100

    feat: use further triangulation to move the implementation in better direction

--- src/main/java/Template/Template.java
+++ src/main/java/Template/Template.java
@@ -2,15 +2,17 @@ package Template;
 
 public class Template {
 
+    private String variableValue;
+
     public Template(String templateText) {
 
     }
 
     public void set(String variable, String value) {
-
+        this.variableValue = value;
     }
 
     public String evaluate() {
-        return "Hello, Reader";
+        return "Hello, " + variableValue;
     }
 }
